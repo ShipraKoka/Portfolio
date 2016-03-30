28647cd23bd4dac1131af8dce0f45faa9dca9caa
Shipra Koka <shipra.koka@gmail.com>
2016-03-29 19:53:21 -0700

    initial commit

---
+++ JavaScript/AskaWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AskaWebsite.Global" Language="C#" %>
---
+++ JavaScript/AskaWebsite/scripts/main.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    var imageName = ["/content/images/afterfive edited small.jpg", "/content/images/active_edited_1.jpg"];
+    var indexNum = 0;
+
+    setInterval(function () {
+        if (indexNum > 1) {
+            indexNum = 0;
+        }
+        $("#change-design").fadeOut(300, function () {
+            $("#change-design").attr("src", imageName[indexNum]);
+            indexNum++;
+
+            $("#change-design").fadeIn(500);
+        })
+    }, 2000);
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+});
