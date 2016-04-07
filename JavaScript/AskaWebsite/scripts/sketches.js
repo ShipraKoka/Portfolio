78720ab9693585af4a61c7fe26584a729b81c34e
Shipra Koka <shipra.koka@gmail.com>
2016-04-06 20:12:27 -0700

    update JavaScript for sketches page

--- JavaScript/AskaWebsite/scripts/sketches.js
+++ JavaScript/AskaWebsite/scripts/sketches.js
@@ -1,1 +1,4 @@
-﻿
+﻿$(document).ready(function () {
+    $("#needle").animate({ left: "200px", opacity: 0.0 }, 1000);
+    $("h1").css("visibility", "visible").animate({ opacity: 1.0, right: "30px" }, 1000);
+});
